e92894f99fee14f2195f0aac44a1428913a866e4
Binil Vincent <binilvincent80@gmail.com>
2025-05-01 19:25:40 +0530

    Update 13-grades.sh

--- Network/code/13-grades.sh
+++ Network/code/13-grades.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 # Program to display student grades  
-while read name marks; do  
-    ((marks >= 90)) && grade=A ||  
-    ((marks >= 80)) && grade=B ||  
-    ((marks >= 70)) && grade=C ||  
-    ((marks >= 60)) && grade=D || grade=F  
-    echo "$name: Grade $grade"  
-done < input.txt  
+
+read -p "Enter Student Name: " name
+read -p "Enter Marks (0 - 100): " marks
+
+if (( marks >= 90 && marks <= 100 )); then
+    grade="A+"
+elif (( marks >= 80 )); then
+    grade="A"
+elif (( marks >= 70 )); then
+    grade="B"
+elif (( marks >= 60 )); then
+    grade="C"
+elif (( marks >= 50 )); then
+    grade="D"
+elif (( marks >= 0 )); then
+    grade="F"
+else
+    echo "Invalid marks!"
+    exit 1
+fi
+
+echo "Student: $name"
+echo "Marks: $marks"
+echo "Grade: $grade"
